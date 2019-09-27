79b9eb1fa2da6f126670f8b32a1699ae34c9f074
Kamran Elchuzade <elchuzade@gmail.com>
2019-09-28 00:49:05 +0300

    added bullets and shooting system, added UI layers and tags

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,72 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+  private float bulletSpeed = 10;
+  private int bulletDamage = 10;
+
   private string direction = "right";
   private string gunDirection = "right";
-  private float walkSpeed = 1;
-  private int jumpPower = 100;
+  private float walkSpeed = 5;
+  private int jumpPower = 500;
   private bool jumping;
+  private float fireRate = 0.1f;
+  [SerializeField] GameObject bulletPrefab;
+  private Transform gameSpace;
 
+  Coroutine shootCoroutine;
+
+  void Awake()
+  {
+    gameSpace = transform.parent.Find("GameSpace");
+  }
   void Start()
   {
-
+    Shoot();
   }
 
   void Update()
   {
     Move();
     Jump();
+  }
+
+  private void Shoot()
+  {
+    shootCoroutine = StartCoroutine(ShootContinuously());
+  }
+
+  IEnumerator ShootContinuously()
+  {
+    while (true)
+    {
+      GameObject bullet = Instantiate(bulletPrefab, transform.position + GetOffset(gunDirection, gameObject), Quaternion.identity);
+      bullet.transform.SetParent(gameSpace);
+      PlayerBullet bulletScript = bullet.GetComponent<PlayerBullet>();
+      bulletScript.Create(bulletSpeed, bulletDamage, gunDirection);
+      yield return new WaitForSeconds(fireRate);
+    }
+  }
+
+  private Vector3 GetOffset(string direction, GameObject player)
+  {
+    Vector3 offset = new Vector3(0, 0, 0);
+    RectTransform rt = (RectTransform)player.transform;
+    switch (direction)
+    {
+      case "right":
+        offset.x = rt.rect.width / 2;
+        break;
+      case "left":
+        offset.x = -rt.rect.width / 2;
+        break;
+      case "up":
+        offset.y = rt.rect.height / 2;
+        break;
+      case "down":
+        offset.y = -rt.rect.height / 2;
+        break;
+    }
+    return offset;
   }
 
   private void Move()
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+  private float speed;
+  private int damage;
+  private string direction;
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    Move();
+  }
+
+  private void Move()
+  {
+    if (direction == "right")
+    {
+      transform.position = new Vector3(transform.position.x + speed, transform.position.y, 0);
+    }
+    else if (direction == "left")
+    {
+      transform.position = new Vector3(transform.position.x - speed, transform.position.y, 0);
+    }
+    else if (direction == "up")
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y + speed, 0);
+    }
+    else if (direction == "down")
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y - speed, 0);
+    }
+  }
+
+  public void Create(float bulletSpeed, int bulletDamage, string bulletDirection)
+  {
+    speed = bulletSpeed;
+    damage = bulletDamage;
+    direction = bulletDirection;
+  }
+
+  void OnCollisionEnter2D(Collision2D collider)
+  {
+    Destroy(gameObject);
+  }
+}
